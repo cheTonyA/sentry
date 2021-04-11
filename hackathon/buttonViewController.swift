01d70db7b4668b1491a1a5d267d6b4ecc9c86ef9
tony <tonyche@tonys-mbp.myfiosgateway.com>
2021-04-10 20:21:40 -0400

    first commit

--- hackathon/buttonViewController.swift
+++ hackathon/buttonViewController.swift
@@ -9,9 +9,90 @@ import UIKit
 
 class buttonViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    
+    @IBOutlet weak var labelOne: UILabel!
+    
+    var seconds = 15
+    var timer = Timer()
+    
+    //function which makes the imer begin
+    func runTimer(){
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(buttonViewController.updateTimer)), userInfo: nil, repeats: true)
+    }
+    
+    
+    @objc func updateTimer(){
+        
+        
+        seconds -= 1
+        label.text = "\(seconds)"
+        
+        //Once seconds(the timer) hits 0 the timer is stoped by timer.invalidate then help is called
+        if seconds == 0 {
+            timer.invalidate()
+            print("call for help")
+            label.text = "HELP IS ON THE WAY!!!"
+            
+        }
+    }
+    
+    @IBAction func button(_ sender: Any) {
+        
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(normalTap))
+        button.addGestureRecognizer(tapGesture)
+
+        let longGesture: UILongPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(longTap))
+        button.addGestureRecognizer(longGesture)
+    }
+    
+
+    @objc func normalTap(_ sender: UIGestureRecognizer){
+        print("Normal tap")
+    }
+    
+    @objc func longTap(_ sender: UIGestureRecognizer){
+        print("Long tap")
+        if sender.state == .ended {
+            
+            runTimer()
+            updateTimer()
+            
+            print("CONTACTS NOTIFIED!!!!")
+            labelOne.text = "EMERGENCY CONTACTS NOTIFIED!!!!"
+            
+            print("UIGestureRecognizerStateEnded")
+            //Do Whatever You want on End of Gesture
+        }
+        else if sender.state == .began {
+            print("UIGestureRecognizerStateBegan.")
+            //Do Whatever You want on Began of Gesture
+        }
+    }
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(normalTap))
+//        button.addGestureRecognizer(tapGesture)
+//
+//        let longGesture: UILongPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(longTap))
+//        button.addGestureRecognizer(longGesture)
+//        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(normalTap))
+//        self.button.addGestureRecognizer(tapGesture)
+//
+//        let longGesture = UILongPressGestureRecognizer(target: self, action: #selector(longTap))
+//        self.button.addGestureRecognizer(longGesture)
+        
 
+       
+        }
+
+        
+        
         // Do any additional setup after loading the view.
     }
     
@@ -26,4 +107,4 @@ class buttonViewController: UIViewController {
     }
     */
 
-}
+
--- hackathon/passwordViewController.swift
+++ hackathon/passwordViewController.swift
@@ -8,14 +8,41 @@
 import UIKit
 
 class passwordViewController: UIViewController {
+    
 
+    @IBOutlet weak var incorrectPassword: UILabel!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var enterButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
+    
+    @IBAction func enterButtonfunc(_ sender: Any) {
+        
+        let password = passwordField.text!
+        //let passwordOne = "password"
+       // let isEqual = (password == passwordOne)
+        
+        
+            if password != "yes" {
+                
+                print("incorrect password")
+                incorrectPassword.text = "Wrong Password"
+                print("Error")
 
+                } else {
+                    print("correct password")
+                    self.performSegue(withIdentifier: "segueToOrigin", sender: nil)
+                    
+                }
+            
+        }
+    
+    
     /*
     // MARK: - Navigation
 
